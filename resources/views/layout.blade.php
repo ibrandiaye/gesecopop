79bb96693270c7d74521f0bdfa27ba0897f5b08b
ibrandiaye <ibra789ndiaye@gmail.com>
2021-07-12 10:43:21 +0000

    second commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,7 +10,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
         <link rel="shortcut icon" href="assets/images/favicon.ico">
-
+        @yield('css')
         <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet" type="text/css">
         <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" type="text/css">
---
+++ resources/views/projet/add.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('css')
+<!-- Plugins css -->
+<link href="assets/plugins/timepicker/tempusdominus-bootstrap-4.css" rel="stylesheet" />
+<link href="assets/plugins/timepicker/bootstrap-material-datetimepicker.css" rel="stylesheet">
+<link href="assets/plugins/clockpicker/jquery-clockpicker.min.css" rel="stylesheet" />
+<link href="assets/plugins/colorpicker/asColorPicker.min.css" rel="stylesheet" type="text/css" />
+<link href="assets/plugins/select2/select2.min.css" rel="stylesheet" type="text/css" />
+@endsection
+@section('content')
+
+@endsection
